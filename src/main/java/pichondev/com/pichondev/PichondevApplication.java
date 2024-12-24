ec0efc8a00811abfdcb12a41a51d027abeb32d2d
EliasAsfoura <eliasasfoura7@gmail.com>
2024-12-24 19:04:17 -0300

    start project

---
+++ src/main/java/pichondev/com/pichondev/PichondevApplication.java
@@ -0,0 +1,13 @@
+package pichondev.com.pichondev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PichondevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PichondevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pichondev/com/pichondev/PichondevApplicationTests.java
@@ -0,0 +1,13 @@
+package pichondev.com.pichondev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PichondevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
